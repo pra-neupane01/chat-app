408ebee379bde01db99ef127229126d11783dda7
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-08 15:47:32 +0545

    feat: add register request and user response DTOs

---
+++ src/main/java/in/praladneupane/chat/auth/dto/request/RegisterRequest.java
@@ -0,0 +1,18 @@
+package in.praladneupane.chat.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record RegisterRequest(
+        @NotBlank(message = "Full name is required")
+        String fullName,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid Email")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        String password
+) {
+}
