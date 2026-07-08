28311c3222af0e0c72090ad807503ebe921023d0
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-08 16:32:39 +0545

    feat: add exceptions and global exception handler

---
+++ src/main/java/in/praladneupane/chat/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package in.praladneupane.chat.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
