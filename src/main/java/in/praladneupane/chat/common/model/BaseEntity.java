58133eb762874973cee2eeddef846239ef7ada63
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-08 15:04:24 +0545

    feat: add base entity with common audit fields

--- src/main/java/in/praladneupane/chat/common/model/BaseEntity.java
+++ src/main/java/in/praladneupane/chat/common/model/BaseEntity.java
@@ -1,4 +1,33 @@
 package in.praladneupane.chat.common.model;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(updatable = true)
+    private LocalDateTime updatedAt;
+
+    @Column(updatable = false)
+    private LocalDateTime deletedAt;
+
+
 }
--- src/main/java/in/praladneupane/chat/user/model/User.java
+++ src/main/java/in/praladneupane/chat/user/model/User.java
@@ -1,4 +1,5 @@
 package in.praladneupane.chat.user.model;
 
+
 public class User {
 }
