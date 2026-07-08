0bfeebee7b807a059b4a20707ec8a1f0990220ed
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-08 16:50:09 +0545

    feat: implement user registration logic

--- src/main/java/in/praladneupane/chat/auth/service/AuthService.java
+++ src/main/java/in/praladneupane/chat/auth/service/AuthService.java
@@ -1,26 +1,32 @@
 package in.praladneupane.chat.auth.service;
 
 import in.praladneupane.chat.auth.dto.request.RegisterRequest;
+import in.praladneupane.chat.common.exception.BusinessException;
 import in.praladneupane.chat.user.dto.response.UserResponse;
 import in.praladneupane.chat.user.mapper.UserMapper;
 import in.praladneupane.chat.user.model.User;
 import in.praladneupane.chat.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserResponse registerUser(RegisterRequest request){
-//        if(userRepository.existsByEmail(request.email())){
-//            throw new
-//
-//        }
+        checkUserExists(request);
         User user = UserMapper.toEntity(request);
+        user.setPassword(passwordEncoder.encode(request.password()));
         User savedUser = userRepository.save(user);
         return UserMapper.toResponse(savedUser);
+    }
+
+    private void checkUserExists(RegisterRequest request) {
+        if(userRepository.existsByEmail(request.email())){
+            throw new BusinessException("User already exists");
+        }
     }
 }
