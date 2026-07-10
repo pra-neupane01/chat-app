9232b7c7701bc4ad0c0e9cbf60fd85a064d55497
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-10 12:53:58 +0545

    feat: add jpa specification executor

--- src/main/java/in/praladneupane/chat/user/repository/UserRepository.java
+++ src/main/java/in/praladneupane/chat/user/repository/UserRepository.java
@@ -3,12 +3,13 @@ package in.praladneupane.chat.user.repository;
 import in.praladneupane.chat.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 import java.util.UUID;
 
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
