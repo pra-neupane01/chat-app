5d1b4b0e45675197471da7233b1d691dc19c9ee5
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-11 19:32:18 +0545

    feat: add repository to find authenticated user convo

--- src/main/java/in/praladneupane/chat/chat/repository/ConversationRepository.java
+++ src/main/java/in/praladneupane/chat/chat/repository/ConversationRepository.java
@@ -1,6 +1,8 @@
 package in.praladneupane.chat.chat.repository;
 
 import in.praladneupane.chat.chat.model.Conversation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,5 @@ import java.util.UUID;
 
 public interface ConversationRepository extends JpaRepository<Conversation, UUID> {
     Optional<Conversation> findByUserOneIdAndUserTwoId(UUID userOneId, UUID userTwoId);
+    Page<Conversation> findByUserOneIdOrUserTwoId(UUID userOneId, UUID userTwoId, Pageable pageable);
 }
