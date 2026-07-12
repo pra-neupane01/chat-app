164cde031ea3d082695cdea8c2da20790f68df13
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-12 21:29:25 +0545

    feat(chat): add ChatMessage entity with sender, receiver, conversation, and status fields

--- src/main/java/in/praladneupane/chat/chat/ChatController.java
+++ src/main/java/in/praladneupane/chat/chat/ChatController.java
@@ -1,5 +1,6 @@
 package in.praladneupane.chat.chat;
 
+import in.praladneupane.chat.chat.model.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
---
+++ src/main/java/in/praladneupane/chat/chat/model/ChatMessage.java
@@ -0,0 +1,45 @@
+package in.praladneupane.chat.chat.model;
+
+import in.praladneupane.chat.common.model.BaseEntity;
+import in.praladneupane.chat.user.model.User;
+import jakarta.persistence.*;
+import jakarta.persistence.criteria.Fetch;
+import jdk.jfr.Timestamp;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "chat_messages")
+public class ChatMessage extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "conversation_id", nullable = false)
+    private Conversation conversation;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private User receiver;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "message_type")
+    private MessageType messageType;
+
+    @Enumerated(EnumType.STRING)
+    private MessageStatus messageStatus;
+
+    @Column(name = "sent_at", nullable = false, updatable = false)
+    private LocalDateTime sentAt;
+}
