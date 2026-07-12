88994747d3424f97b835e83bfd505170125b28a8
prakash69-code <neupaneprakash9999@gmail.com>
2026-07-12 19:09:25 +0545

    feat: add web socket config

--- src/main/java/in/praladneupane/chat/common/config/WebSocketConfig.java
+++ src/main/java/in/praladneupane/chat/common/config/WebSocketConfig.java
@@ -11,12 +11,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("http://localhost:5173")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic");
+        registry.setUserDestinationPrefix("/user");
     }
 }
